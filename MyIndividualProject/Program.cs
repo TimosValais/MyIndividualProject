698f609e9d7457cf2c682b09930bc8bc76f3acb5
Timos Valais <73531450+TimosValais@users.noreply.github.com>
2020-11-11 00:11:16 +0200

    Fixed some issues, added a readline, removed comments

--- MyIndividualProject/Program.cs
+++ MyIndividualProject/Program.cs
@@ -14,63 +14,12 @@ namespace MyIndividualProject
         {
 
             OrderOfOperations start = new OrderOfOperations();
-            //CommandPromptUtils cpUtils = new CommandPromptUtils();
-            //CourseUtils courseUtils = new CourseUtils();
-            //StudentUtils studentUtils = new StudentUtils();
-            ////Console.WriteLine(trainer);
-            ////Console.WriteLine(trainer2);
-            ////List<Trainer> trainers = new List<Trainer>();
-            ////trainers.AddcpUtils.GetTrainerDetails();
-            ////List<Student> students = new List<Student>();
-            //List<Course> courses = new List<Course>();
-            //courses = courseUtils.GetListOfCourses();
-            //List<Student> students = new List<Student>();
-            //students = studentUtils.GetListOfStudents(courses);
-
-            //CourseStudents studernsPerCourse = new CourseStudents();
-
-            ////List<Student> students = new List<Student>();
-            ////students = cpUtils.GetListOfStudents(courses);
-            //foreach (var item in courses)
-            //{
-                
-            //    Console.WriteLine($"This course:\n {item} \nHas these students :\n");
-            //    List <string> listOfStudents = studernsPerCourse.StudentsPerCourse(item, students);
-            //    for (int i = 0; i < listOfStudents.Count; i++)
-            //    {
-            //        Console.WriteLine($"{listOfStudents[i]}\n");
-
-            //    }
-            //}
-
-            //cpUtils.CheckForDuplicateCourses(courses);
-
-            //courseUtils.CheckForDuplicateCourses(courses);
-            //courseUtils.PrintCoursesList(courses);
-
-
-
-            //foreach (var item in students)
-            //{
-            //    Console.WriteLine(item);
-
-            //}
-
-
-
-            //students.Add(cpUtils.GetStudentDetails());
-
-            //cpUtils.PrintTrainersList(trainers);
-            //List<Course> courses = new List<Course>();
-            //courses.Add(cpUtils.GetCourseDetails());
-            //cpUtils.PrintCoursesList(courses);
+            Console.ReadLine();
 
 
 
         }
 
 
-        // Create appropriate methods in order to ask
-        // from the user data for the four main classes
     }
 }
